2a9810f7d9be8c87e64a162ec304383ee2a8745c
valen725 <juana.martinez@utp.edu.co>
2025-06-26 10:07:01 -0500

    Initial commit
    Set up project structure and add initial files.

--- chapter_2/src_2/batch_generator.py
+++ chapter_2/src_2/batch_generator.py
@@ -70,7 +70,7 @@ def write_to_csv(file_path: str, rows: int) -> None:
         rows (int): The number of fake user records to generate.
     """
     # Create a Faker instance with Romanian data.
-    fake = create_data("ro_RO")
+    fake = create_data("es_MX")
     
     # Define the CSV headers.
     headers = [
@@ -137,7 +137,8 @@ if __name__ == "__main__":
     logging.info(f"Started batch processing for {date.today()}.")
 
     # Define the output file name with today's date.
-    output_file = f"chapter_2/work_2/data_2/batch_{date.today()}.csv"
+    output_file = f"chapter_2/src_2/data_2/batch_{date.today()}.csv"
+
 
     # Define number of records: first run - 10_372; next runs random number.
     if str(date.today()) == "2024-09-14":
